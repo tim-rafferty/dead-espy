f95a60903f5f47677b50fee60e86cb1a6461910a
m33pz0r <107590809+m33pz0r@users.noreply.github.com>
2025-02-22 19:13:47 -0600

    Commiting updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export const content = [
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
 
